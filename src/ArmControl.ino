215a3fc82202a77135192ffe2d2025ba411303f3
ar3vind <aravindbinuraj05@gmail.com>
2025-08-29 15:59:05 +0100

    Initialize ArmControl.ino file for robotic arm project

---
+++ src/ArmControl.ino
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+// ======= Pin configuration =======
+
+const uint8_t PIN_SERVO_BASE     = 3;   // D3
+const uint8_t PIN_SERVO_SHOULDER = 5;   // D5
+const uint8_t PIN_SERVO_ELBOW    = 6;   // D6
+const uint8_t PIN_SERVO_WRIST    = 9;   // D9
+const uint8_t PIN_SERVO_GRIPPER  = 10;  // D10
+
+
+const uint8_t PIN_JOY_X1 = A0;  // Base
+const uint8_t PIN_JOY_Y1 = A1;  // Shoulder
+const uint8_t PIN_JOY_X2 = A2;  // Elbow
+const uint8_t PIN_JOY_Y2 = A3;  // Wrist
+const uint8_t PIN_GRIP_POT = A4; // Gripperr
+
+
+const uint8_t PIN_GRIP_BTN = 7;  
+
+void setup() {}
+void loop() {}
